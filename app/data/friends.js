5618589b7a7ca5de9ca51f028545c1e61bd7594b
M L <mlecontenyc@gmail.com>
2019-01-07 13:23:24 -0500

    work in progress

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+
+//friendArray data holds info for all friends
+
+var friendArray = [
+    {
+        "name":"John",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+          ]
+      },
+      {
+        "name":"Paul",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            4,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"Ringo",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            2,
+            2,
+            4,
+            4,
+            4,
+            4,
+            2,
+            4,
+            3
+          ]
+      },
+      {
+        "name":"George",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            3,
+            3,
+            3,
+            2,
+            2,
+            2,
+            2,
+            2,
+            3
+          ]
+      }
+  ];
+  
+  //export the friendArray which makes it accessible to other files
+  module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,24 @@
+
+//npm package dependency
+var express = require("express");
+
+//creating server
+var app = express();
+
+//establishing port
+var PORT = process.env.PORT || 7500;
+
+//data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//routers which will respond to user request
+ require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//lisening to port
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
