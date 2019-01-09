44e39d792dc305db20089cd0bf672078ad84108b
M L <mlecontenyc@gmail.com>
2019-01-08 19:19:55 -0500

    final touches

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@
 var friendArray = [
     {
         "name":"John",
-        "photo":"https://en.wikipedia.org/wiki/John_Lennon#/media/File:John_Lennon_rehearses_Give_Peace_A_Chance_cropped.jpg",
+        "photo":"assets/images/john.png",
         "scores":[
             4,
             4,
@@ -20,50 +20,50 @@ var friendArray = [
       },
       {
         "name":"Paul",
-        "photo":"https://en.wikipedia.org/wiki/Paul_McCartney#/media/File:Paul_McCartney_-_Out_There_Concert_-_140420-5941-jikatu_(13950091384).jpg",
+        "photo":"assets/images/paul.png",
         "scores":[
-            1,
-            1,
-            1,
-            4,
             5,
             5,
             5,
             5,
-            1,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
           ]
       },
       {
         "name":"Ringo",
-        "photo":"https://en.wikipedia.org/wiki/Ringo_Starr#/media/File:Ringo_Starr_and_all_his_band_(8470866906).jpg",
+        "photo":"assets/images/ringo.png",
         "scores":[
             2,
             2,
             2,
-            4,
-            4,
-            4,
-            4,
             2,
-            4,
-            3
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
           ]
       },
       {
         "name":"George",
-        "photo":"https://en.wikipedia.org/wiki/George_Harrison#/media/File:George_Harrison_1974_edited.jpg",
+        "photo":"../assets/images/george.png",
         "scores":[
-            4,
-            3,
-            3,
-            3,
-            2,
-            2,
-            2,
-            2,
-            2,
-            3
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
           ]
       }
   ];
